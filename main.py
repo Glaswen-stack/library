08ac24fdde860dec7bda85b35fdfcc05884b0895
Glaswen-stack <usatenkor@mail.ru>
2026-05-25 12:47:47 +0300

    Stage 3

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import json
 import os
 
 
+from typing import Optional
+
+from pydantic import ValidationError
+from schemas import Book, BookSell, Transaction, Author
+
+
 def load_data(filename: str) -> list:
     if os.path.isfile(filename):
         with open(filename, "r", encoding="utf-8") as f:
@@ -15,11 +21,13 @@ def load_data(filename: str) -> list:
             json.dump([], f, ensure_ascii=False, indent=4)
         return []
 
-def save_data(data: list, filename: str) -> list | None:
+
+def save_data(data: list, filename: str) -> Optional[list]:
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-def find_book(book_name: str) -> int | None:
+
+def find_book(book_name: str) -> Optional[int]:
     books = load_data("book.json")
     for i, book in enumerate(books):
         if book["book_name"] == book_name:
@@ -27,8 +35,7 @@ def find_book(book_name: str) -> int | None:
     return None
 
 
-
-def find_author(author_name: str) -> int | None:
+def find_author(author_name: str) -> Optional[int]:
     authors = load_data("authors.json")
     for i, author in enumerate(authors):
         if author["author_name"] == author_name:
@@ -36,75 +43,64 @@ def find_author(author_name: str) -> int | None:
     return None
 
 
-def buy_book(book_name: str, count: int, buy_price: int, sell_price: int, author_name: str, genre: str, language: str, year: int) -> None:
-    transactions_data = {
-        "book_name": book_name,
-        "count": count,
-        "transaction_type": "buy",
-        "price": buy_price,
-    }
+def buy_book(book: Book) -> None:
+    transactions_data = Transaction(
+        book_name=book.book_name,
+        count=book.count,
+        transaction_type="buy",
+        price=book.buy_price,
+    )
 
     transactions = load_data("transactions.json")
-    transactions.append(transactions_data)
+    transactions.append(transactions_data.model_dump())
     save_data(transactions, "transactions.json")
 
-    #РАБОТА С КНИГАМИ
-    book_data = {
-        "book_name": book_name,
-        "count": count,
-        "buy_price": buy_price,
-        "sell_price": sell_price,
-        "author_name": author_name,
-        "genre": genre,
-        "language": language,
-        "year": year,
-    }
+    # РАБОТА С КНИГАМИ
 
     books = load_data("book.json")
-    index = find_book(book_name)
+    index = find_book(book.book_name)
     if index is not None:
-        books[index]["count"] += count
+        books[index]["count"] += book.count
     else:
-        books.append(book_data)
+        books.append(book.model_dump())
 
     save_data(books, "book.json")
 
-    #РАБОТА С АВТОРАМИ
-    author_data = {
-        "author_name": author_name,
-    }
+    # РАБОТА С АВТОРАМИ
+    author_data = Author(
+        author_name=book.author_name,
+    )
 
     authors = load_data("authors.json")
-    author_index = find_author(author_name)
+    author_index = find_author(book.author_name)
     if author_index is None:
-        authors.append(author_data)
+        authors.append(author_data.model_dump())
         save_data(authors, "authors.json")
 
 
-def sell_book(book_name: str, count: int) -> None:
+def sell_book(book: BookSell) -> None:
     books = load_data("book.json")
-    index = find_book(book_name)
+    index = find_book(book.book_name)
     if index is None:
-        raise ValueError(f"Книга {book_name} не найдена!")
+        raise ValueError(f"Книга {book.book_name} не найдена!")
 
-    elif books[index]["count"] < count:
-        raise ValueError (f"Книг {book_name} недостаточно для продажи")
+    elif books[index]["count"] < book.count:
+        raise ValueError(f"Книг {book.book_name} недостаточно для продажи")
     else:
-        books[index]["count"] -= count
-        print(f"{book_name} было продано {count} шт!")
+        books[index]["count"] -= book.count
+        print(f"{book.book_name} было продано {book.count} шт!")
         price = books[index]["sell_price"]
         save_data(books, "book.json")
-        transactions_data = {
-            "book_name": book_name,
-            "count": count,
-            "transaction_type": "sell",
-            "price": price,
-        }
+        transactions_data = Transaction(
+            book_name=book.book_name,
+            count=book.count,
+            transaction_type="sell",
+            price=price,
+        )
 
         transactions = load_data("transactions.json")
-        transactions.append(transactions_data)
+        transactions.append(transactions_data.model_dump())
         save_data(transactions, "transactions.json")
-
 
 
 # ------------------------------------------
@@ -113,25 +109,51 @@ def sell_book(book_name: str, count: int) -> None:
 if __name__ == "__main__":
     print("Начинаем симуляцию магазина...")
 
-    # 1. Закупаем книги на склад
-    buy_book(
-        book_name="Python Crash Course2",
-        count=10,
-        buy_price=1000,
-        sell_price=1111,
-        author_name="author",
-        genre="python",
-        language="en",
-        year=2001,
-    )
-     # 2. Продаем пару книг
-    sell_book(book_name="Python Crash Course2", count=3)
-    sell_book(book_name="Python Crash Course2", count=2)
+    buy_info = {
+        "book_name": "Python Crash Course2",
+        "count": 10,
+        "buy_price": 1000,
+        "sell_price": 1111,
+        "author_name": "21",
+        "genre": "python",
+        "language": "en",
+        "year": 2001,
+    }
+    book_class = Book(**buy_info)
 
-    # 3. Пытаемся продать больше, чем есть на складе
     try:
-        sell_book(book_name="Python Crash Course2", count=100)
+        buy_book_class = Book(**buy_info)
+        buy_book(buy_book_class)
+    except ValidationError:
+        print("Ошибка валидации")
+
+    sell_info_1 = {
+        "book_name": "Python Crash Course2",
+        "count": 2,
+    }
+    sell_info_2 = {
+        "book_name": "Python Crash Course2",
+        "count": 3,
+    }
+    sell_info_3 = {
+        "book_name": "Python Crash Course2",
+        "count": 10,
+    }
+
+    try:
+        sell_book_class_1 = BookSell(**sell_info_1)
+        sell_book_class_2 = BookSell(**sell_info_2)
+        sell_book_class_3 = BookSell(**sell_info_3)
+
+        sell_book(sell_book_class_1)
+        sell_book(sell_book_class_2)
+
+        # 3. Пытаемся продать больше, чем есть на складе
+
+        sell_book(sell_book_class_3)
         print("ОШИБКА: Твой код позволил продать 100 книг, хотя на складе их всего 5!")
+    except ValidationError:
+        print("Ошибка валидации")
     except ValueError:
         print("ОТЛИЧНО: Твой код правильно заблокировал продажу (выдал ValueError).")
 
---
+++ schemas.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field, model_validator, ValidationError
+
+
+class Book(BaseModel):
+    book_name: str = Field(min_length=1, max_length=200)
+    count: int = Field(ge=0)
+    buy_price: int = Field(gt=0)
+    sell_price: int = Field(gt=0)
+    author_name: str = Field(min_length=2, max_length=200)
+    genre: str = Field(min_length=2, max_length=100)
+    language: str = Field(min_length=2, max_length=50)
+    year: int = Field(ge=1000, le=2026)
+
+    @model_validator(mode="after")
+    def check_profit(self):
+        if self.sell_price <= self.buy_price:
+            raise ValidationError("Цена продажи должна быть больше цены покупки")
+        return self
+
+
+class BookSell(BaseModel):
+    book_name: str = Field(min_length=1, max_length=200)
+    count: int = Field(gt=0)
+
+
+class Transaction(BaseModel):
+    book_name: str = Field(min_length=1, max_length=200)
+    count: int = Field(ge=0)
+    transaction_type: str = Field(min_length=1, max_length=200)
+    price: int = Field(gt=0)
+
+
+class Author(BaseModel):
+    author_name: str = Field(min_length=1, max_length=200)
