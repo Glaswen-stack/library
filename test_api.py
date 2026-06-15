5228c8e35fe478464fa9c2f861c9b3f63adf7162
Usatenko Roman <usatenkoroman@MacBook-Air-Usatenko.local>
2026-06-15 13:07:08 +0300

    Дописаны все тесты ручек

--- test_api.py
+++ test_api.py
@@ -2,7 +2,7 @@ import json
 import urllib.request
 import urllib.error
 
-from service import find_book, find_author, load_data
+from service import load_data
 
 BASE_URL = "http://127.0.0.1:8000"
 
@@ -84,6 +84,39 @@ def test_purchase():
         print(f"❌ Ошибка в books/purchase: транзакция не найдена")
 
 
+def test_sell_book():
+    url = f"{BASE_URL}/books/sell"
+    data = {
+        "book_name": "Test",
+        "count": 1,
+    }
+    json_data = json.dumps(data).encode("utf-8")
+    req = urllib.request.Request(url, data=json_data, method="POST")
+    req.add_header("Content-Type", "application/json")
+    try:
+        with urllib.request.urlopen(req) as response:
+            assert response.status == 200
+            print(f"✅ POST /books/sell ручка исполнена")
+    except Exception as e:
+        print(f"❌ Ошибка в books/sell: {e}")
+
+
+def test_get_book_by_id():
+    book_id = 1
+    url = f"{BASE_URL}/books/{book_id}"
+    req = urllib.request.Request(url, method="GET")
+    try:
+        with urllib.request.urlopen(req) as response:
+            assert response.status == 200
+            data = response.read().decode("utf-8")
+            book = json.loads(data)
+            assert book.get("book_name") == "Test"
+            assert "book_name" in book
+            print(f"✅ Книга с id={book_id} найдена: {book['book_name']}")
+    except urllib.error.HTTPError as e:
+        print(f"❌ Ошибка /books/book_id {e.code}: {e.reason}")
+
+
 def test_books_list():
     req = urllib.request.Request(f"{BASE_URL}/books")
     try:
@@ -126,10 +159,30 @@ def test_transaction_list():
         print(f"❌ Ошибка в /transactions: {e}")
 
 
+def test_profit():
+    req = urllib.request.Request(f"{BASE_URL}/profit", method="GET")
+    try:
+        with urllib.request.urlopen(req) as response:
+            assert response.status == 200
+            data = response.read().decode("utf-8")
+            profit_data = json.loads(data)
+            assert "revenue" in profit_data
+            assert "expenses" in profit_data
+            assert "profit" in profit_data
+            print(
+                f"✅ GET /profit работает: revenue={profit_data['revenue']}, expenses={profit_data['expenses']}, profit={profit_data['profit']}"
+            )
+    except Exception as e:
+        print(f"❌ Ошибка в GET /profit: {e}")
+
+
 if __name__ == "__main__":
     print("Запуск проверок API...")
     test_health()
     test_purchase()
+    test_sell_book()
     test_books_list()
+    test_get_book_by_id()
     test_transaction_list()
     test_inventory()
+    test_profit()
